0d36b9beee3d4de20a1619e1aeadc180fc94cb13
LucyMacko <118434024+LucyMacko@users.noreply.github.com>
2023-05-09 09:36:02 +0000

    Final version

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("domain").innerHTML = generateDomain();
+};
+
+const generateDomain = () => {
+  let pronoun = ["the", "our", "this"];
+  let adj = ["Great", "Empowering", "Amazing", "Cool", "Cuddly"];
+  let noun = ["Thinker", "Dragon", "Unicorn", "Wizard", "Witcher"];
+  let extensions = [".com", ".net", ".us", ".io", ".sk", ".perfectProject"];
+
+  return (
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+    adj[Math.floor(Math.random() * adj.length)] +
+    noun[Math.floor(Math.random() * noun.length)] +
+    extensions[Math.floor(Math.random() * extensions.length)]
+  );
 };
